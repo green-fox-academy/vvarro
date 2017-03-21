856a4312a6f4c1e4ad8092f6cab59f0ec59dc3ef
VVarro <virag.varro@gmail.com>
2017-03-21 16:50:58 +0100

    create and modify FavouriteNumber.java

---
+++ week-02/day-02/src/FavouriteNumber.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Virág on 3/21/2017.
+ */
+public class FavouriteNumber {
+  public static void main(String[] args) {
+    int favouriteNumber = 8;
+    System.out.println("My favorite number is: " + favouriteNumber);
+  }
+}
