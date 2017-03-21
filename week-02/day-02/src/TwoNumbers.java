2cc2178a48fb6d492c3fe025d1e12d362041407c
VVarro <virag.varro@gmail.com>
2017-03-21 14:33:54 +0100

    create and modify TwoNumbers.java

---
+++ week-02/day-02/src/TwoNumbers.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Virág on 3/21/2017.
+ */
+public class TwoNumbers {
+  public static void main(String[] args) {
+    int firstNumber = 13;
+    int secondNumber = 22;
+    double add = firstNumber + secondNumber;
+    double substr = firstNumber - secondNumber;
+    double multiply = secondNumber * firstNumber;
+    double divide = secondNumber / firstNumber;
+    double integ = secondNumber / firstNumber;
+    double reminder = secondNumber % firstNumber;
+    System.out.println(add);
+    System.out.println(substr);
+    System.out.println(multiply);
+    System.out.println(divide);
+    System.out.println(integ);
+    System.out.println(reminder);
+  }
+}
