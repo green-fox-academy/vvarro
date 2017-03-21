d31d612d1fed4ca7fa30717865245defaadaad39
VVarro <virag.varro@gmail.com>
2017-03-21 13:47:33 +0100

    change Helloworld.java

---
+++ week-02/day-02/src/Hellovirag.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Virág on 3/21/2017.
+ */
+public class Hellovirag {
+}
--- week-02/day-02/src/Helloworld.java
+++ week-02/day-02/src/Helloworld.java
@@ -2,10 +2,7 @@
  * Created by Virág on 3/21/2017.
  */
 public class Helloworld {
-    public static void main(String[] args) {
-        System.out.printf("ALMA");
-        int apples = 123;
-
-        System.out.println(apples);
-        }
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
