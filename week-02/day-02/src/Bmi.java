9bbc54f9fe7c5a2f36b5772aa17f27c2c306123b
VVarro <virag.varro@gmail.com>
2017-03-21 17:09:29 +0100

    create and modify Bmi.java

---
+++ week-02/day-02/src/Bmi.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Virág on 3/21/2017.
+ */
+public class Bmi {
+  public static void main(String[] args) {
+    double massInKg = 81.2;
+    double heightInM = 1.78;
+    System.out.println("Body mass index (BMI): " + massInKg / heightInM);
+  }
+}
