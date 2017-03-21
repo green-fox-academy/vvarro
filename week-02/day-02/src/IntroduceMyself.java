d14e2108d5eb5262e9a58431b547d4a2d3be2095
VVarro <virag.varro@gmail.com>
2017-03-21 14:10:14 +0100

    create and modify IntroduceMyself.java

---
+++ week-02/day-02/src/IntroduceMyself.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Virág on 3/21/2017.
+ */
+public class IntroduceMyself {
+  public static void main(String[] args) {
+    String name = "Jane Doe";
+    int age = 28;
+    double height = 1.73;
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(height);
+  }
+}
