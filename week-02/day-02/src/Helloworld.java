cc7a8cdaf0ce8b08017269f0b87bbc1b5e280820
VVarro <virag.varro@gmail.com>
2017-03-21 13:16:21 +0100

    add Helloworld.java

---
+++ week-02/day-02/src/Helloworld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Virág on 3/21/2017.
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.printf("ALMA");
+        int apples = 123;
+
+        System.out.println(apples);
+        }
+    }
