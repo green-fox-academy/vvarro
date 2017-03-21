cf6124291652a5b1d3fade85c59ae6023773fa62
VVarro <virag.varro@gmail.com>
2017-03-21 16:57:20 +0100

    create and modify DefineBasicInfo.java

---
+++ week-02/day-02/src/DefineBasicInfo.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Virág on 3/21/2017.
+ */
+public class DefineBasicInfo {
+  public static void main(String[] args) {
+    String name = "Jane Doe";
+    int age = 28;
+    double height = 1.73;
+    boolean married = false;
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(height);
+    System.out.println(married);
+  }
+}
