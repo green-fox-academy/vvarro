daa00c92080a255a14b10a740f01632b0f860b84
VVarro <virag.varro@gmail.com>
2017-03-21 13:58:40 +0100

    create and modify HelloOthers.java

---
+++ week-02/day-02/src/HelloOthers.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Virág on 3/21/2017.
+ */
+public class HelloOthers {
+    public static void main(String[] args) {
+      System.out.println("Hello, Andi!");
+      System.out.println("Hello, Balázs!");
+      System.out.println("Hello, Levi!");
+    }
+}
