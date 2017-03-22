30f6cecc06d2e52810cdbecbda2a397e50d63bb0
VVarro <virag.varro@gmail.com>
2017-03-22 21:02:32 +0100

    fix the problem with spaces - Triangle2.java

--- week-02/day-03/src/Triangle2.java
+++ week-02/day-03/src/Triangle2.java
@@ -9,7 +9,7 @@ public class Triangle2 {
     Scanner scanner = new Scanner(System.in);
     int number = scanner.nextInt();
     for (int i = 0; i < number; i++) {
-      for (int j = (number / 2 - 1); j >= i; j--) {
+      for (int j = (number - 1); j > i; j--) {
         System.out.print(" ");
       }
       for (int k = 0; k < 2 * i + 1; k++) {
