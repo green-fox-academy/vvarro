c91af339bc0c9e84b525e3982321efe8dca345ab
VVarro <virag.varro@gmail.com>
2017-03-22 15:10:30 +0100

    create Diamond.java

---
+++ week-02/day-03/src/Diamond.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * Created by Virág on 3/22/2017.
+ */
+public class Diamond {
+  public static void main(String[] args) {
+    System.out.println("Enter a number: ");
+    Scanner scanner = new Scanner(System.in);
+    int number = scanner.nextInt();
+    for (int i = 0; i < number; i++) {
+      if (i <= number / 2)
+        for (int j = (number/2 - 1); j >= i; j--) {
+          System.out.print(" ");
+        }
+      else
+        for (int j = number+number/2+1 ; j <= number+i ; j++) {
+          System.out.print(" ");
+        }
+      if (i <= number / 2)
+        for (int k = 0; k < 2*i+1; k++) {
+          System.out.print("*");
+        }
+      else
+        for (int k = 0; k < (number - i) + ((number - i)-1)  ; k++) {
+          System.out.print("*");
+        }
+      System.out.println(" ");
+    }
+  }
+}
