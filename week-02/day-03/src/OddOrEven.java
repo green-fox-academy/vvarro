3f81ffde8aadd0317af14b21fb0b9db3f978a2d9
VVarro <virag.varro@gmail.com>
2017-03-22 11:41:10 +0100

    create OddOrEven.java

---
+++ week-02/day-03/src/OddOrEven.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Created by Virág on 3/22/2017.
+ */
+public class OddOrEven {
+  public static void main(String[] args) {
+      System.out.println("Enter a number: ");
+      Scanner scanner = new Scanner(System.in);
+      int number = scanner.nextInt();
+      if ((number % 2) == 0)
+        System.out.println("The number is even");
+      else
+        System.out.println("The number is odd");
+    }
+  }
