74d4a4093174c35f6218daed1afab4f8fda663df
VVarro <virag.varro@gmail.com>
2017-03-22 15:18:32 +0100

    create Doubling.java

---
+++ week-02/day-03/src/Doubling.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Virág on 3/22/2017.
+ */
+public class Doubling {
+  public static void main(String[] args) {
+    doubling();
+  }
+
+  public static void doubling() {
+    int ak = 123;
+    System.out.println(2*ak);
+  }
+}
