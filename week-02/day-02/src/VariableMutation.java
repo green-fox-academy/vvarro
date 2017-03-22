81c0c73702be3253d0720b482d801fd45713cd43
VVarro <virag.varro@gmail.com>
2017-03-22 11:23:41 +0100

    create VariableMutation.java

---
+++ week-02/day-02/src/VariableMutation.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Virág on 3/21/2017.
+ */
+public class VariableMutation {
+  public static void main(String[] args) {
+    int a = 3;
+    a = a + 10;
+    System.out.println(a);
+    int b = 100;
+    b = b - 7;
+    System.out.println(b);
+    int c = 44;
+    c = 2 * c;
+    System.out.println(c);
+    int d = 125;
+    d = d / 5;
+    System.out.println(d);
+    int e = 8;
+    e = e * e * e;
+    System.out.println(e);
+    int f1 = 123;
+    int f2 = 345;
+
+  }
+}
