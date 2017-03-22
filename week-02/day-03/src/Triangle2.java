a364651e9d44db40e5fc6e45b83eaebbd206b954
VVarro <virag.varro@gmail.com>
2017-03-22 13:22:22 +0100

    modify: fix the number of spaces on Triangle2.java

--- week-02/day-03/src/Triangle2.java
+++ week-02/day-03/src/Triangle2.java
@@ -12,8 +12,8 @@ public class Triangle2 {
       for (int j = (number - 1); j >= i; j--) {
         System.out.print(" ");
       }
-      for (int k = 0; k <= i; k++) {
-        System.out.print("* ");
+      for (int k = 0; k < 2*i+1; k++ ) {
+        System.out.print("*");
       }
       System.out.println(" ");
     }
