744d6daaa89641c37d057861c5fd3fd2793d088d
VVarro <virag.varro@gmail.com>
2017-03-22 11:55:48 +0100

    create Repeat100Times.java

---
+++ week-02/day-03/src/Repeat100Times.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Virág on 3/22/2017.
+ */
+public class Repeat100Times {
+  public static void main(String[] args) {
+    for (int i = 0; i < 100; i++) {
+      System.out.println("I won't cheat on the exam!");
+    }
+  }
+}
