1cb3331c5862970b9c619f404c5b22c24c1fb234
VVarro <virag.varro@gmail.com>
2017-03-22 15:41:01 +0100

    create Greeting.java

---
+++ week-02/day-03/src/Greeting.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Virág on 3/22/2017.
+ */
+public class Greeting {
+  public static void main(String[] args) {
+    String al = "Greenfox";
+    greet(al);
+  }
+
+  public static void greet(String al) {
+    System.out.println("Greetings dear, " + al);
+  }
+}
