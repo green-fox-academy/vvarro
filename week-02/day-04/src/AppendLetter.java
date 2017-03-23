97c029acf564d4ce202d0ec2ef0eb7a29f718269
VVarro <virag.varro@gmail.com>
2017-03-23 23:00:47 +0100

    fix code Appendletter.java

--- week-02/day-04/src/AppendLetter.java
+++ week-02/day-04/src/AppendLetter.java
@@ -1,12 +1,17 @@
 import java.util.*;
+
 /**
  * Created by Virág on 3/23/2017.
  */
-public class AppendLetter{
-  public static void main(String... args){
+public class AppendLetter {
+  public static void main(String... args) {
     ArrayList<String> far = new ArrayList<String>(Arrays.asList("kuty", "macsk", "kacs", "rók", "halacsk"));
-    for (String i : far) {
-      far.add(i+"a");
+    String farAdd = new String ();
+    int farLength = far.size();
+    for (int i = 0; i < farLength; i++) {
+      farAdd = far.get(i);
+      farAdd = farAdd.concat("a");
+      far.set(i, farAdd);
     }
     System.out.println(far);
   }
