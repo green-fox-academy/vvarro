23ae38e912f6506476b7e06e062e2dccd3408820
VVarro <virag.varro@gmail.com>
2017-03-23 16:45:29 +0100

    insert Saturnus into the right place in SolarSystem.java

--- week-02/day-04/src/SolarSystem.java
+++ week-02/day-04/src/SolarSystem.java
@@ -1,8 +1,12 @@
+import java.util.*;
+
 /**
  * Created by Virág on 3/23/2017.
  */
-public class SolarSystem }
-  public static void main(String[]args){
-
+public class SolarSystem {
+  public static void main(String... args) {
+    ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"));
+    planetList.add(5, "Saturn");
+    System.out.println(planetList);
+  }
 }
-        }
