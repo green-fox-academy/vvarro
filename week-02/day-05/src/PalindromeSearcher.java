a3f89e03356d89c4e6551a8e50bdd7512dc6ec8e
VVarro <virag.varro@gmail.com>
2017-03-24 00:15:40 +0100

    convert string to arraylist

--- week-02/day-05/src/PalindromeSearcher.java
+++ week-02/day-05/src/PalindromeSearcher.java
@@ -1,7 +1,18 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 /**
  * Created by Virág on 3/24/2017.
  */
 public class PalindromeSearcher {
+  public static void main(String[] args) {
+    System.out.println("Enter the text: ");
+    Scanner Scanner = new Scanner(System.in);
+    String Input = Scanner.nextLine();
+    ArrayList<Character> chars = new ArrayList<Character>();
+    for (char c : Input.toCharArray()) {
+      chars.add(c);
+    }
+    
+    System.out.println(chars);
+  }
 }
