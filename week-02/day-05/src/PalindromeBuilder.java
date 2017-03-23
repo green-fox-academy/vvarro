360caa2cfb4a5da558026d117104e01e72bebb49
VVarro <virag.varro@gmail.com>
2017-03-23 23:59:24 +0100

    create PalindromeBuilder.java

---
+++ week-02/day-05/src/PalindromeBuilder.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Created by Virág on 3/23/2017.
+ */
+public class PalindromeBuilder {
+  public static void main(String... args) {
+    System.out.println("Enter the text: ");
+    Scanner Scanner = new Scanner(System.in);
+    String Input = Scanner.nextLine();
+    System.out.println(rev(Input));
+  }
+
+  public static String rev(String Input) {
+    int length = Input.length();
+    String a = "";
+    for (int i = length - 1; i >= 0; i--) {
+      a = a + Input.charAt(i);
+    }
+    a = Input.concat(a);
+    return a;
+  }
+}
