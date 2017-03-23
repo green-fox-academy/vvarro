0cf4749313137623b5123e626cc717a0918157df
VVarro <virag.varro@gmail.com>
2017-03-23 12:19:04 +0100

    create SimpleReplace.java

---
+++ week-02/day-04/src/SimpleReplace.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Virág on 3/23/2017.
+ */
+public class SimpleReplace {
+    public static void main(String... args) {
+      String example = "In a dishwasher far far away";
+      System.out.println (example.replace("dishwasher", "galaxy"));
+    }
+}
