0ce9835c98cf81bf5d101b63a8bfa2de53efd078
VVarro <virag.varro@gmail.com>
2017-03-24 14:02:28 +0100

    finish PalindromeSearcher.java

--- week-02/day-05/src/PalindromeSearcher.java
+++ week-02/day-05/src/PalindromeSearcher.java
@@ -8,12 +8,23 @@ public class PalindromeSearcher {
     System.out.println("Enter the text: ");
     Scanner Scanner = new Scanner(System.in);
     String Input = Scanner.nextLine();
-    ArrayList<String> palindrome = new ArrayList<String>();
-    palindromise(Input, palindrome);
-    realPalindrome(palindrome);
+    ArrayList<String> palindrome = new ArrayList<>();
+    palindrome = palindromise(Input);
+    int p = 0;
+    for (int i = 0; i < palindrome.size(); i++) {
+      String result = palindrome.get(i);
+      if (realPalindrome(result)) {
+        System.out.print("\"" + result + "\" ");
+        p = 1;
       }
+      }
+    if (p == 0 ) {
+      System.out.print("[]");
+    }
+    }
 
-  public static void palindromise(String Input, ArrayList palindrome) {
+  public static ArrayList palindromise(String Input) {
+    ArrayList<String> palindrome = new ArrayList<String>();
     String in = "";
     char first = ' ';
     char last = ' ';
@@ -24,22 +35,20 @@ public class PalindromeSearcher {
         if (first == last) {
           in = Input.substring(i, j + 1);
           palindrome.add(in);
-
         }
       }
     }
+    return palindrome;
   }
 
-  public static boolean realPalindrome(ArrayList palindrome) {
-    String sPalindrom = "";
+  public static boolean realPalindrome(String result) {
     char pFirst = ' ';
     char pLast = ' ';
-    for (int i = 0; i < palindrome.size(); i++) {
-      sPalindrom = palindrome.get(i).toString();
-      for (int j = 0; j < sPalindrom.length(); j++) {
-        pFirst = sPalindrom.charAt(j);
-        pLast = sPalindrom.charAt(sPalindrom.length() - 1 - j);
-        if ((pFirst != pLast) && (j < sPalindrom.length() - 1 - j)) {
+    for (int i = 0; i < result.length(); i++) {
+      for (int j = 0; j < result.length(); j++) {
+        pFirst = result.charAt(j);
+        pLast = result.charAt(result.length() - 1 - j);
+        if ((pFirst != pLast) && (j < result.length() - 1 - j)) {
           return false;
         }
       }
