43551cfb1feedfc25440e7ddb3dad29bfd9ad31e
VVarro <virag.varro@gmail.com>
2017-03-24 10:54:00 +0100

    new function - delete duplicated palindroms

--- week-02/day-05/src/PalindromeSearcher.java
+++ week-02/day-05/src/PalindromeSearcher.java
@@ -12,7 +12,29 @@ public class PalindromeSearcher {
     for (char c : Input.toCharArray()) {
       chars.add(c);
     }
-    
-    System.out.println(chars);
+    ArrayList<String> palindrome = new ArrayList<String>();
+    String in = "";
+    char first = ' ';
+    char last = ' ';
+    for (int i = 0; i < Input.length(); i++) {
+      first = Input.charAt(i);
+      for (int j = i + 1; j < Input.length(); j++) {
+        last = Input.charAt(j);
+        if (first == last) {
+          in = Input.substring(i, j + 1);
+          palindrome.add(in);
+        }
+      }
+    }
+    for (int k = 0; k < palindrome.size(); k++) {
+      for (int l = k + 1; l < palindrome.size(); l++) {
+        if (palindrome.get(k).equals(palindrome.get(l))) {
+          palindrome.remove(l);
+          l--;
+        }
+      }
+    }
+    System.out.println(palindrome);
+
   }
 }
