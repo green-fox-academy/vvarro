27e2450144a4889364e2c0ec44594876545ac8dc
VVarro <virag.varro@gmail.com>
2017-03-27 16:38:10 +0200

    Writes out the first line - Doubled.java

--- week-03/day-01/src/Doubled.java
+++ week-03/day-01/src/Doubled.java
@@ -6,23 +6,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * Created by Virág on 3/27/2017.
- */
 public class Doubled {
   public static void main(String[] args) throws IOException {
     Path path = Paths.get("day-01/duplicated-chars.txt");
     List<String> lines = Files.readAllLines(path);
     System.out.println(lines);
     ArrayList<String> words = new ArrayList<>(lines);
-    System.out.println(words.get(0));
-    for (int i = 0; i < words.size(); i++) {
-      for (int j = 0; j < words.size(); j++) {
-        if (words.get(i).indexOf(j) == words.get(i).indexOf(j + 1)) {
-          System.out.println("!");
+    for (int i = 0; i < lines.size(); i++) {
+      for (int j = 0; j >= i ; j++) {
+        if ((words.get(i).charAt(j)) == (words.get(i).charAt(j + 1))) {
+          System.out.print(words.get(i).charAt(j + 1));
         }
       }
     }
-    System.out.println(words);
   }
 }
