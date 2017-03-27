e7c358e89555db5dcf828baa339796095eedc00f
VVarro <virag.varro@gmail.com>
2017-03-27 15:06:11 +0200

    Reading and compare function fixed - Doubled.java

--- week-03/day-01/src/Doubled.java
+++ week-03/day-01/src/Doubled.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -9,8 +11,18 @@ import java.util.List;
  */
 public class Doubled {
   public static void main(String[] args) throws IOException {
-    Path path = Paths.get("day-01/duplicated-chars.csv");
+    Path path = Paths.get("day-01/duplicated-chars.txt");
     List<String> lines = Files.readAllLines(path);
     System.out.println(lines);
+    ArrayList<String> words = new ArrayList<>(lines);
+    System.out.println(words.get(0));
+    for (int i = 0; i < words.size(); i++) {
+      for (int j = 0; j < words.size(); j++) {
+        if (words.get(i).indexOf(j) == words.get(i).indexOf(j + 1)) {
+          System.out.println("!");
+        }
+      }
+    }
+    System.out.println(words);
   }
 }
