8be34ce3d04df6cb62f7810c485d3f85b9ade340
VVarro <virag.varro@gmail.com>
2017-03-28 23:50:24 +0200

    func(summing) it summes the numbers

--- week-02/day-03/src/Summing.java
+++ week-02/day-03/src/Summing.java
@@ -1,8 +1,17 @@
-/**
- * Created by Virág on 3/28/2017.
- */
+import java.util.Scanner;
+
 public class Summing {
   public static void main(String[] args) {
-
+    System.out.println("Please give me a number: ");
+    Scanner scanner = new Scanner(System.in);
+    int number = scanner.nextInt();
+  }
+  public static int Summer() {
+    int summary = 0;
+    for (int i = 0; i <= number; i++) {
+      summary = summary + i;
+    }
+    return summary;
   }
 }
+
