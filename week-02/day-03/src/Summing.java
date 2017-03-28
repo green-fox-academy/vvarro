834f50b5e6a80c47a6d9e1a05caee0ee15a06a1b
VVarro <virag.varro@gmail.com>
2017-03-29 00:21:17 +0200

    fix the structure of the function

--- week-02/day-03/src/Summing.java
+++ week-02/day-03/src/Summing.java
@@ -5,15 +5,10 @@ public class Summing {
     System.out.println("Please give me a number: ");
     Scanner scanner = new Scanner(System.in);
     int number = scanner.nextInt();
-<<<<<<< HEAD
     System.out.println(summary(number));
   }
 
   public static int summary(int number) {
-=======
-  }
-  public static int Summer() {
->>>>>>> 8be34ce3d04df6cb62f7810c485d3f85b9ade340
     int summary = 0;
     for (int i = 0; i <= number; i++) {
       summary = summary + i;
