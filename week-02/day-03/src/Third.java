c5e76182380d50691814bf728be6dc41a43f3a89
VVarro <virag.varro@gmail.com>
2017-03-28 23:18:22 +0200

    create Doubled.java

---
+++ week-02/day-03/src/Third.java
@@ -0,0 +1,11 @@
+import java.lang.reflect.Array;
+
+/**
+ * Created by Virág on 3/28/2017.
+ */
+public class Third {
+  public static void main(String[] args) {
+    int q[] = {4, 5, 6, 7};
+    System.out.println("The third element of q is:"+ q[2]);
+  }
+}
