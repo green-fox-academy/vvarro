0c73f3f5504302533a6586b74d8dde3d304a0980
VVarro <virag.varro@gmail.com>
2017-03-29 01:03:27 +0200

    create Printer.java

---
+++ week-02/day-03/src/Printer.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Virág on 3/29/2017.
+ */
+public class Printer {
+  public static void main(String[] args) {
+    String[] stringArray = {"first", "second", "third"};
+  }
+  public static String printer(String[] stringArray){
+    
+  }
+}
