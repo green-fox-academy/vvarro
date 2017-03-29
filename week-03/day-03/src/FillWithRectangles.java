f4e135693f66a0779bbd7868e761580b0a4f1b2c
VVarro <virag.varro@gmail.com>
2017-03-29 12:13:59 +0200

    finish FillWithRectangles.java

--- week-03/day-03/src/FillWithRectangles.java
+++ week-03/day-03/src/FillWithRectangles.java
@@ -1,7 +1,39 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
-/**
- * Created by Virág on 3/29/2017.
- */
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
 public class FillWithRectangles {
+
+  public static void mainDraw(Graphics graphics){
+    // draw four different size and color rectangles.
+    graphics.setColor(Color.BLUE);
+    graphics.fillRect(30, 40, 10, 60);
+    graphics.setColor(Color.GREEN);
+    graphics.fillRect(40, 100, 50, 10);
+    graphics.setColor(Color.RED);
+    graphics.fillRect(50, 40, 80, 30);
+    graphics.setColor(Color.PINK);
+    graphics.fillRect(100, 140, 30, 60);
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("FillWithRectangles");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
 }
