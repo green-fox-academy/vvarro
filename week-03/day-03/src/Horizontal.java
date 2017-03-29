6886ba1f2f185500d8406ad682c34dd13e96c3e8
VVarro <virag.varro@gmail.com>
2017-03-29 11:40:43 +0200

    finish Horizontal.java

--- week-03/day-03/src/Horizontal.java
+++ week-03/day-03/src/Horizontal.java
@@ -9,9 +9,9 @@ public class Horizontal {
     // the x and y coordinates of the line's starting point
     // and draws a 50 long horizontal line from that point.
     // draw 3 lines with that function.
-    drawLine(graphics, 120, 100);
-    drawLine(graphics, 12, 200);
-    drawLine(graphics, 10, 200);
+    drawLine(graphics, 100, 100);
+    drawLine(graphics, 12, 150);
+    drawLine(graphics, 10, 100);
   }
     public static void drawLine(Graphics graphics,int coordinateX, int coordinateY){
       graphics.drawLine(coordinateX, coordinateY, coordinateX + 50, coordinateY);
