4e050dc8cda490bd9841f8f2eca253cb736de980
VVarro <virag.varro@gmail.com>
2017-03-29 11:33:52 +0200

    finish ToTheCenter.java

--- week-03/day-03/src/ToTheCenter.java
+++ week-03/day-03/src/ToTheCenter.java
@@ -1,11 +1,22 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.Scanner;
-
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class ToTheCenter  {
-  //    Don't touch the code below
+public class ToTheCenter {
+  public static void mainDraw(Graphics graphics) {
+    // create a line drawing function that takes 2 parameters:
+    // the x and y coordinates of the line's starting point
+    // and draws a line from that point to the center of the canvas.
+    // draw 3 lines with that function.
+    drawLine(graphics, 20, 20);
+    drawLine(graphics, 120, 30);
+    drawLine(graphics, 220, 30);
+  }
+
+  public static void drawLine(Graphics graphics,int coordinateX, int coordinateY){
+    graphics.drawLine(coordinateX, coordinateY, 150, 150);
+  }
+
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("ToTheCenter");
     jFrame.setSize(new Dimension(300, 300));
@@ -13,26 +24,13 @@ public class ToTheCenter  {
     jFrame.add(new ImagePanel());
     jFrame.setLocationRelativeTo(null);
     jFrame.setVisible(true);
-    mainDraw(cordinateX, cordinateY, 150, 150);
   }
-  static class ImagePanel extends JPanel{
+
+  static class ImagePanel extends JPanel {
     @Override
     protected void paintComponent(Graphics graphics) {
       super.paintComponent(graphics);
       mainDraw(graphics);
     }
-  }
-  public static void mainDraw(Graphics graphics){
-    // create a line drawing function that takes 2 parameters:
-    // the x and y coordinates of the line's starting point
-    // and draws a line from that point to the center of the canvas.
-    // draw 3 lines with that function.
-    System.out.println("Please give me the X coordinate: ");
-    Scanner xScanner = new Scanner(System.in);
-    int coordinateX = xScanner.nextInt();
-    System.out.println("Please give me the Y coordinate: ");
-    Scanner yScanner = new Scanner(System.in);
-    int coordinateY = yScanner.nextInt();
-    graphics.drawLine(coordinateX, coordinateY , 150, 150);
   }
 }
