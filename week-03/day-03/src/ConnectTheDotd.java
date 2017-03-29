89cc5b6883b064a595014aa1c8de77760985c151
VVarro <virag.varro@gmail.com>
2017-03-29 17:40:36 +0200

    function vol1.

--- week-03/day-03/src/ConnectTheDotd.java
+++ week-03/day-03/src/ConnectTheDotd.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 
 import java.awt.*;
 import java.lang.reflect.Array;
+import java.util.*;
+import java.util.List;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
@@ -16,16 +18,20 @@ public class ConnectTheDotd {
     // {120, 100}, {85, 130}, {50, 100}}
       drawPolygon(graphics, {{10, 10}, {290,  10}, {290, 290}, {10, 290}});
     }
-  }
+  int[][] points = new int[][];
   public static void drawPolygon(Graphics graphics, int[][] points){
+    ArrayList<Integer> polyX = new ArrayList<>();
+    ArrayList<Integer> polyY = new ArrayList<>();
+for (int i = 0; i < points.length; i++ ) {
+    polyX.set(i, points[i][0]);
+    polyY.set(i, points[0][i]);
+    }
     int[] x = points[][] {10, 290, 290, 10};
     int[] y = {10, 10, 290, 290};
     int n = points.length();
     graphics.drawPolygon(x, y, n);
   }
 
-
-  }
 
   //    Don't touch the code below
   public static void main(String[] args) {
