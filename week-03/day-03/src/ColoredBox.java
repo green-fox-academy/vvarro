7f251f0d127d7db3842c7c73e18ab52286a81f3a
VVarro <virag.varro@gmail.com>
2017-03-29 10:11:53 +0200

    create ColoredBox.java

--- week-03/day-03/src/ColoredBox.java
+++ week-03/day-03/src/ColoredBox.java
@@ -1,7 +1,30 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
 
-/**
- * Created by Virág on 3/29/2017.
- */
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
 public class ColoredBox {
+  public static void mainDraw(Graphics graphics) {
+    // draw a box that has different colored lines on each edge.
+
+
+  }
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("ColoredBox");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
 }
