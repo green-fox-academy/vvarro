3f6cc5df15e6ccc9c62e9e71984c7ad41c732d01
VVarro <virag.varro@gmail.com>
2017-03-29 15:37:03 +0200

    finish FunctionToCenter.java

---
+++ week-03/day-03/src/FunctionToCenter.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class FunctionToCenter {
+
+  public static void mainDraw(Graphics graphics) {
+    // create a line drawing function that takes 2 parameters:
+    // the x and y coordinates of the line's starting point
+    // and draws a line from that point to the center of the canvas.
+    // fill the canvas with lines from the edges, every 20 px, to the center.
+    for (int i=0; i<15; i++) {
+      drawLine(graphics, 0, (i+1)*20);
+    }
+  }
+    public static void drawLine(Graphics graphics,int coordinateX, int coordinateY){
+      graphics.drawLine(coordinateX, coordinateY, 150, 150);
+    }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("FunctionToCenter");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
