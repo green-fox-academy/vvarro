be7e58eefc74bd93ae142c3ff202d06c08a0384b
VVarro <virag.varro@gmail.com>
2017-03-30 10:27:26 +0200

    create Counter.java

---
+++ week-03/day-04/src/Counter.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.util.Scanner;
+
+    // Write a recursive function that takes one parameter: n and counts down from n.
+    public class Counter {
+      public static void main(String[] args) {
+        System.out.println("Please give me a number:");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(countDown(n));
+      }
+
+      public static int countDown(int num) {
+        if (num == 0) {
+          return 0;
+        } else {
+          System.out.println(num);
+          return countDown(num - 1);
+        }
+      }
+    }
