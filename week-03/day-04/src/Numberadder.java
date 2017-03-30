7a1bb64a9e69fa2f3d61b4c74736ae866c929372
VVarro <virag.varro@gmail.com>
2017-03-30 10:40:46 +0200

    finish sum function at Numberadder.java

--- week-03/day-04/src/Numberadder.java
+++ week-03/day-04/src/Numberadder.java
@@ -1,6 +1,19 @@
-/**
- * Created by Virág on 3/30/2017.
- */
-public class Numberadder {
 // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+import java.util.Scanner;
+
+public class Numberadder {
+public static void main(String[] args) {
+  System.out.print("Please give me a number: ");
+  Scanner scanner = new Scanner(System.in);
+  int n = scanner.nextInt();
+  System.out.println(sum(n));
+}
+
+  public static int sum(int n) {
+    if (n == 0) {
+      return 0;
+    } else {
+      return n + sum(n - 1);
+    }
+  }
 }
