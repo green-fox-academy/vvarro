ae8107ab6d4485ce0e80dfb25f0bdb18bdb22abf
VVarro <virag.varro@gmail.com>
2017-03-30 12:48:46 +0200

    create StringsAgain.java

---
+++ week-03/day-04/src/StringsAgain.java
@@ -0,0 +1,23 @@
+// Given a string, compute recursively a new string where all the 'x' chars have been removed.
+
+import java.util.Scanner;
+
+public class StringsAgain {
+  public static void main(String[] args) {
+    System.out.print("Please give me a text:");
+    Scanner scanner = new Scanner(System.in);
+    String text = scanner.nextLine();
+    char charX = 'x';
+    System.out.println(replace(text, charX));
+  }
+
+  public static String replace(String textRep, char charX) {
+    if (textRep.length() < 1) {
+      return textRep;
+    }
+    if (textRep.charAt(0) == charX) {
+      textRep = "" + textRep.substring(1);
+    }
+    return textRep.charAt(0) + replace(textRep.substring(1, textRep.length()), charX);
+  }
+}
