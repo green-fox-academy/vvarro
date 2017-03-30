c592966482ae5487e80b4a7e82a6a84821f4c463
VVarro <virag.varro@gmail.com>
2017-03-30 10:29:38 +0200

    create Numberadder.java

--- week-03/day-04/src/Numberadder.java
+++ week-03/day-04/src/Numberadder.java
@@ -1,7 +1,6 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Virág on 3/30/2017.
  */
 public class Numberadder {
+// Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
 }
