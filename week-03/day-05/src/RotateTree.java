df0adf4daa69e2e33666ae08a5f3599e2e1556c3
VVarro <virag.varro@gmail.com>
2017-03-31 13:39:40 +0200

    mini project-rotate the tree levels

--- week-03/day-05/src/RotateTree.java
+++ week-03/day-05/src/RotateTree.java
@@ -1,7 +1,40 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
 
-/**
- * Created by Virág on 3/31/2017.
- */
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
 public class RotateTree {
+  public static void mainDraw(Graphics graphics) {
+    drawTree(graphics, 150, 300, 40, 15, 10, 1);
+    graphics.drawLine(150, 300 + 40, 150, 300);
+  }
+
+  public static void drawTree(Graphics graphics, int x, int y, int lenght, int moveX, int moveY, int repeat) {
+      graphics.drawLine(x, y, x - moveX, y - lenght + moveY);//bal
+      graphics.drawLine(x, y, x, y - lenght );//kozepso
+      graphics.drawLine(x, y, x + moveX, y - lenght + moveY);//jobb
+
+      graphics.drawLine(x - moveX, y - lenght + moveY, x - moveX*2 - moveX, y - lenght + moveY - lenght + moveY*2);//bal
+      graphics.drawLine(x - moveX, y - lenght + moveY, x-moveX-moveX, y - lenght + moveY - lenght + moveY);//kozepso
+      graphics.drawLine(x - moveX, y - lenght + moveY, x - moveX*2 + moveX , y - lenght+ moveY - lenght+moveY);//jobb
+    }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("RotateTree");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+
 }
