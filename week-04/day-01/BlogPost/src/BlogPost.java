4dee904f4d61563211d5058936976da6efa99eca
VVarro <virag.varro@gmail.com>
2017-04-03 11:37:24 +0200

    create BlogPost class

---
+++ week-04/day-01/BlogPost/src/BlogPost.java
@@ -0,0 +1,14 @@
+
+public class BlogPost {
+  public String authorName;
+  public String title;
+  public String text;
+  public String publicationDate;
+
+  BlogPost(String authorName, String title, String text, String publicationDate) {
+    this.authorName = authorName;
+    this.title = title;
+    this.text = text;
+    this.publicationDate = publicationDate;
+  }
+}
