bcf80299a3cd8b63daba29ed75449b82c78aad3a
VVarro <virag.varro@gmail.com>
2017-04-03 14:19:38 +0200

    finish domino orderer function

--- week-04/day-01/Dominoes/src/DominoOrder.java
+++ week-04/day-01/Dominoes/src/DominoOrder.java
@@ -3,11 +3,11 @@ import java.util.List;
 
 public class DominoOrder {
   public static List<Domino> order(List<Domino> domino) {
-    Domino replace = new Domino(0, 0);
+    Domino replace = new Domino(0,0);
     for (int i = 0; i < domino.size(); i++) {
-      for (int j = 0; j < domino.size(); j++) {
+      for (int j = i + 1; j < domino.size(); j++) {
         if (domino.get(i).getValues()[1] == domino.get(j).getValues()[0]) {
-          replace = domino.get(i);
+          replace = domino.get(i + 1);
           domino.set(i+1, domino.get(j));
           domino.set(j, replace);
         }
