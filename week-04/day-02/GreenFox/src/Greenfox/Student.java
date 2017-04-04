a5aa7796e1ac7a833e69febae19fcdcdccd2e439
VVarro <virag.varro@gmail.com>
2017-04-04 12:15:52 +0200

    create new constructors in Student class

--- week-04/day-02/GreenFox/src/Greenfox/Student.java
+++ week-04/day-02/GreenFox/src/Greenfox/Student.java
@@ -5,13 +5,30 @@ public class Student extends Person {
   private int skippedDays;
 
   @Override
-  public void getGoal(){
+  public void getGoal() {
     System.out.println("Be a junior software developer.");
   }
 
   @Override
-  public void introduce(){
-    System.out.println("Hi, I'm" + getName() + " a " + getAge() + "year old" + getGender() + " from " + previousOrganization + " who skipped " + skippedDays + "days from the course already.");
+  public void introduce() {
+    System.out.println("Hi, I'm" + this.getName() + " a " + this.getAge() + "year old" + this.getGender() + " from " + previousOrganization + " who skipped " + skippedDays + "days from the course already.");
   }
+
+  public void skipDays(int numberOfDays) {
+    skippedDays += numberOfDays;
+  }
+
+  Student() {
+    super();
+    this.previousOrganization = "The school of life";
+    this.skippedDays = 0;
+  }
+
+  Student(String name, int age, String gender, String previousOrganization) {
+    super(name, age, gender);
+    this.previousOrganization = previousOrganization;
+    this.skippedDays = 0;
+  }
+
 
 }
