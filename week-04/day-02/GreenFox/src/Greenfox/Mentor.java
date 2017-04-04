31e2be202546e964c30795c766a076b9a56061af
VVarro <virag.varro@gmail.com>
2017-04-04 12:29:34 +0200

    create new Menor class

---
+++ week-04/day-02/GreenFox/src/Greenfox/Mentor.java
@@ -0,0 +1,25 @@
+package Greenfox;
+
+public class Mentor extends Person {
+  String level;
+
+  @Override
+  public void getGoal() {
+    System.out.println("Educate brilliant junior software developers.");
+  }
+
+  @Override
+  public void introduce() {
+    System.out.println("Hi, I'm" + this.getName() + " a " + this.getAge() + "year old" + this.getGender() + level + " mentor.");
+  }
+
+  Mentor() {
+    super();
+    this.level = "intermediate";
+  }
+
+  Mentor(String name, int age, String gender, String level) {
+    super(name, age, gender);
+    this.level = level;
+  }
+}
