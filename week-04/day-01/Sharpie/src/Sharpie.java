a482315a46f87183391521982918e63fdda71fc4
VVarro <virag.varro@gmail.com>
2017-04-04 22:22:24 +0200

    create Sharpie.java

--- week-04/day-01/Sharpie/src/Sharpie.java
+++ week-04/day-01/Sharpie/src/Sharpie.java
@@ -1,4 +1,16 @@
 
 public class Sharpie {
+  String color;
+  double width;
+  double inkAmount;
 
+  public void use() {
+    inkAmount -= 5;
+  }
+
+  public Sharpie(String color, double width) {
+    this.color = color;
+    this.width = width;
+    this.inkAmount = 100.0;
+  }
 }
