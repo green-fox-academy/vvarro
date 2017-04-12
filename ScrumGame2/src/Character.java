6558f7c5d8e14b2d5d2fd0dc721a3b2c0b1959e3
VVarro <virag.varro@gmail.com>
2017-04-12 15:57:09 +0200

    fix movev methods

--- ScrumGame2/src/Character.java
+++ ScrumGame2/src/Character.java
@@ -3,26 +3,29 @@ public class Character extends GameObject {
     super(posX, posY, costume);
   }
 
+  public Character() {
+  }
+
   private void moveUp() {
-    if (GameMap.mapStructure[getPosY() / 72 - 1][getPosX() / 72] != 0) {
+    if ((GameMap.mapStructure[getPosY() / 72 - 1][getPosX() / 72] != 0) && (getPosY() - 72 >= 0)) {
       move(0, -72);
     }
   }
 
   private void moveDown() {
-    if (GameMap.mapStructure[getPosY() / 72 + 1][getPosX() / 72] != 0) {
+    if ((GameMap.mapStructure[getPosY() / 72 + 1][getPosX() / 72] != 0) && (getPosY() + 72 < 720)) {
       move(0, +72);
     }
   }
 
   private void moveLeft() {
-    if (GameMap.mapStructure[getPosY() / 72][getPosX() / 72 - 1] != 0) {
+    if ((GameMap.mapStructure[getPosY() / 72][getPosX() / 72 - 1] != 0) && (getPosX() - 72 >= 0)) {
       move(-72, 0);
     }
   }
 
   private void moveRight() {
-    if (GameMap.mapStructure[getPosY() / 72][getPosX() / 72 + 1] != 0) {
+    if ((GameMap.mapStructure[getPosY() / 72][getPosX() / 72 + 1] != 0) && (getPosX() + 72 < 720)) {
       move(+72, 0);
     }
   }
