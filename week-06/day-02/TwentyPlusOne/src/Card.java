33da72a2725cb2c97588d9d3c5dd4226304bc226
VVarro <virag.varro@gmail.com>
2017-04-19 16:34:45 +0200

    create Card methods: pullFirst, pullLast, pullRandom, shuffle

--- week-06/day-02/TwentyPlusOne/src/Card.java
+++ week-06/day-02/TwentyPlusOne/src/Card.java
@@ -4,10 +4,10 @@ public class Card {
     TWO(2), THREE(3), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), J(10), Q(10), K(10), A
       (11);
 
-    private int RankValue;
+    private int Rankpoint;
 
-    Rank(int value) {
-      this.RankValue = value;
+    Rank(int point) {
+      this.Rankpoint = point;
     }
   }
 
@@ -22,20 +22,14 @@ public class Card {
   }
 
   public enum Color {
-    BLACK(Color.BLACK), RED(Color.RED);
-
-    private Color color;
-
-    Color(Color color) {
-      this.color = color;
-    }
+    BLACK, RED
   }
 
   private final Rank rank;
   private final Suit suit;
   private final Color color;
 
-  private Card(Rank rank, Suit suit, Color color) {
+  public Card(Rank rank, Suit suit, Color color) {
     this.rank = rank;
     this.suit = suit;
     this.color = color;
--- week-06/day-02/TwentyPlusOne/src/Deck.java
+++ week-06/day-02/TwentyPlusOne/src/Deck.java
@@ -1,2 +1,48 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Deck {
+  private static final List<Card> Deck = new ArrayList<Card>();
+
+  public Deck() {
+    for (Card.Color color : Card.Color.values()) {
+      for (Card.Suit suit : Card.Suit.values()) {
+        for (Card.Rank rank : Card.Rank.values()) {
+          Deck.add(new Card(rank, suit, color));
+        }
+      }
+    }
+  }
+
+  public static ArrayList<Card> newDeck() {
+    return new ArrayList<Card>(Deck);
+  }
+
+  public String toString(){
+    return Deck.toString();
+  }
+
+  public void shuffleDeck() {
+    Collections.shuffle(Deck);
+  }
+
+  public Card pullFirst() {
+    if (Deck.size() == 0) {
+      return null;
+    }
+    Card removed = Deck.remove(0);
+    return removed;
+  }
+
+  public Card pullLast() {
+    Card removed = Deck.remove(Deck.size()-1);
+    return removed;
+  }
+
+  public Card pullRandom() {
+    Card removed = Deck.remove((int)Math.random()*10);
+    return removed;
+  }
+
 }
