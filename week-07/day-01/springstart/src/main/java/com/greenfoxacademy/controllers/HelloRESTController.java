be4e357d4f2686f80c8370f6f2935f40e2018623
VVarro <virag.varro@gmail.com>
2017-05-02 13:18:07 +0200

    new feature: counter

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/controllers/HelloRESTController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/controllers/HelloRESTController.java
@@ -1,16 +1,17 @@
 package com.greenfoxacademy.controllers;
 
 import com.greenfoxacademy.Greeting;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
-
+  AtomicLong counter = new AtomicLong();
   @RequestMapping(value = "/greeting")
   public Greeting greeting(@RequestParam String name){
-    Greeting greeting = new Greeting(1, "Hello, " + name + "!");
+    Greeting greeting = new Greeting(counter.incrementAndGet(), "Hello, " + name + "!");
     return greeting;
   }
 }
