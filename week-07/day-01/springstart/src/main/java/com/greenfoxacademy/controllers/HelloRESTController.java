cb8962ad9c0e66510ca11aa1cb317b646de801ff
VVarro <virag.varro@gmail.com>
2017-05-02 13:11:12 +0200

    improve app: use user name to greeting

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/controllers/HelloRESTController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/controllers/HelloRESTController.java
@@ -2,14 +2,15 @@ package com.greenfoxacademy.controllers;
 
 import com.greenfoxacademy.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
   @RequestMapping(value = "/greeting")
-  public Greeting greeting(){
-    Greeting greeting = new Greeting(1, "Hello, World!");
+  public Greeting greeting(@RequestParam String name){
+    Greeting greeting = new Greeting(1, "Hello, " + name + "!");
     return greeting;
   }
 }
