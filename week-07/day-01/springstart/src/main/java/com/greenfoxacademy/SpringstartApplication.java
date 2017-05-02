88f7611db5bcadd028c1e96996f0bea77ef76b81
VVarro <virag.varro@gmail.com>
2017-05-02 11:36:05 +0200

    create HelloWorld in Spring

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/SpringstartApplication.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/SpringstartApplication.java
@@ -1,14 +1,18 @@
 package com.greenfoxacademy;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+
 public class SpringstartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringstartApplication.class, args);
-		@Controller
 	}
 }
