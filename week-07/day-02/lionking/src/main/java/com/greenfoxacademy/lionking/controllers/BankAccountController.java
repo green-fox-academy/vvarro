4c08a61c85adfa9f237f3dca7973e1a38737cb3c
VVarro <virag.varro@gmail.com>
2017-05-03 16:55:22 +0200

    owner of the BankAccount is a Bad Guy, or a Good One with switch

--- week-07/day-02/lionking/src/main/java/com/greenfoxacademy/lionking/controllers/BankAccountController.java
+++ week-07/day-02/lionking/src/main/java/com/greenfoxacademy/lionking/controllers/BankAccountController.java
@@ -101,4 +101,15 @@ public class BankAccountController {
     model.addAttribute("characters",characters);
     return "bankaccount8";
   }
+
+  @RequestMapping("/excersise9")
+  public String isBadGuySwitch(Model model) {
+    List<BankAccount> characters = new ArrayList<>();
+    characters.add(new BankAccount("Nala", 3000, "lion" ));
+    characters.add(new BankAccount("Mufasa", 8000, "lion" ));
+    characters.add(new BankAccount("Timon", 2000, "meerkat" ));
+    characters.add(new BankAccount("Pumbaa", 1000, "warthog" ));
+    model.addAttribute("characters",characters);
+    return "bankaccount9";
+  }
 }
