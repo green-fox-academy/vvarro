4e53b33f1ccdb72ed1f0b6bf24412480e260eba4
VVarro <virag.varro@gmail.com>
2017-05-15 12:27:37 +0200

    add tests

--- week-09/day-01/iamgroot/src/main/java/com/greenfoxacademy/iamgroot/controller/GuardianController.java
+++ week-09/day-01/iamgroot/src/main/java/com/greenfoxacademy/iamgroot/controller/GuardianController.java
@@ -1,6 +1,8 @@
 package com.greenfoxacademy.iamgroot.controller;
 
 import com.greenfoxacademy.iamgroot.model.GrootMessage;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,5 +14,11 @@ public class GuardianController {
   public GrootMessage getMessage(@RequestParam(value = "message") String received){
     GrootMessage grootMessage = new GrootMessage(received);
     return grootMessage;
+  }
+  @ExceptionHandler(MissingServletRequestParameterException.class)
+  public com.greenfoxacademy.iamgroot.controller.ExceptionController getError(
+      MissingServletRequestParameterException ex) {
+    return new com.greenfoxacademy.iamgroot.controller.ExceptionController(
+        "I am Groot!");
   }
 }
---
+++ week-09/day-01/iamgroot/src/test/java/com/greenfoxacademy/iamgroot/GuardianControllerTest.java
@@ -0,0 +1,51 @@
+package com.greenfoxacademy.iamgroot;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = IamgrootApplication.class)
+@WebAppConfiguration
+@EnableWebMvc
+public class GuardianControllerTest {
+
+  private MockMvc mockMvc;
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+
+  @Before
+  public void setup() throws Exception {
+    this.mockMvc = webAppContextSetup(webApplicationContext).build();
+  }
+
+  @Test
+  public void WithQueryParam_ReceiveGrootGreet() throws Exception {
+    mockMvc.perform(get("/groot?message=somemessage"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.received").value("somemessage"))
+        .andExpect(jsonPath("$.translated").value("I am Groot!"));
+  }
+
+  @Test
+  public void WithoutQueryParam_ReceiveGrootGreet() throws Exception {
+    mockMvc.perform(get("/groot"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.error").value("I am Groot!"));
+  }
+}
