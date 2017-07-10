0d46de805b6def02a95fb1244384f7da214106e4
VVarro <virag.varro@gmail.com>
2017-07-10 09:45:16 +0200

    specify return value type

--- finalexam/src/LetterFinder.java
+++ finalexam/src/LetterFinder.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LetterFinder {
-  public List findLetter(String text, char letter) {
+  public List<Integer> findLetter(String text, char letter) {
     List<Integer> indexes = new ArrayList<>();
     for (int i = 0; i < text.length(); i++) {
       if (text.charAt(i) == letter) {
