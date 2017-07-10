99a0e7b8f9ef94f169cafc394c73fe0006cadeb4
VVarro <virag.varro@gmail.com>
2017-07-10 10:00:07 +0200

    new sollution: use findCharLetter

--- finalexam/src/LetterFinder.java
+++ finalexam/src/LetterFinder.java
@@ -2,13 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LetterFinder {
-  public List<Integer> findLetter(String text, char letter) {
+  public List<Integer> findCharLetter(String text, char letter) {
     List<Integer> indexes = new ArrayList<>();
     for (int i = 0; i < text.length(); i++) {
       if (text.charAt(i) == letter) {
         indexes.add(i);
       }
     }
+    return indexes;
+  }
+
+  public List<Integer> findCharLetterMethod(String text, char letter) {
+    List<Integer> indexes = new ArrayList<>();
+    indexes = findCharLetter(text, letter);
     return indexes;
   }
 }
--- finalexam/src/Main.java
+++ finalexam/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
   public static void main(String[] args) {
     LetterFinder letterfinder = new LetterFinder();
-    System.out.println(letterfinder.findLetter("finalexam", 'a'));
+    System.out.println(letterfinder.findCharLetter("finalexam", 'a'));
+    System.out.println(letterfinder.findCharLetterMethod("finalexam", 'a'));
   }
 }
